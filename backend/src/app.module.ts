450700370c8574d5bdcbbd999320e9caa821f8af
dounia-elg <dounia.elg20@gmail.com>
2026-02-23 13:57:15 +0000

    TRIQ-1 feat: restructure Users module and move related files to new directory

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -15,8 +16,10 @@ import { AppService } from './app.service';
         uri: config.get<string>('MONGO_URI'),
       }),
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
+ 
 })
 export class AppModule {}
---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>,
+  ) {}
+
+  async findAll() {
+    return this.userModel.find();
+  }
+}
