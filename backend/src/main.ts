e6a58d1c16d56ec82c64ca1b664b5208766f651e
dounia-elg <dounia.elg20@gmail.com>
2026-03-24 14:36:53 +0100

    TRIQ-40 fix: enable CORS for local development

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,11 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
